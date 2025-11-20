22b9e4d58c3ed59553beafdea0cd8986487010cb
Yander9 <afonkin.georgi@gmail.com>
2025-11-20 21:55:12 +0200

    Update send.php

--- nginx-lab/www/send.php
+++ nginx-lab/www/send.php
@@ -1,18 +1,11 @@
 <?php
+require 'vendor/autoload.php';
 require 'QueueManager.php';
 
-if ($_POST) {
-    $q = new QueueManager();
-    $data = [
-        'name' => $_POST['name'] ?? 'Без имени',
-        'email' => $_POST['email'] ?? '',
-        'timestamp' => date('Y-m-d H:i:s'),
-        'type' => 'user_registration'
-    ];
-    
-    $q->publish($data);
-    
-    echo "<h2>✅ Сообщение отправлено в Kafka!</h2>";
-    echo "<pre>" . json_encode($data, JSON_PRETTY_PRINT) . "</pre>";
-    echo '<a href="/">Назад</a>';
-}
+$q = new QueueManager();
+$q->publish([
+    'name' => $_POST['name'] ?? 'Без имени',
+    'timestamp' => date('Y-m-d H:i:s')
+]);
+
+echo "✅ Сообщение отправлено в Kafka!";
